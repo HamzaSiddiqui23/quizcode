cfc29800347050030dde586bb3af0439f7997c41
Hamza Siddiqui <Hamza Siddiqui>
2016-03-11 00:12:49 +0500

    Commit 2

--- mycode.go
+++ mycode.go
@@ -1,8 +1,14 @@
 package main
 
-import "github.com/HamzaSiddiqui23/pkg"
+import (
+	"fmt"
+
+	"github.com/HamzaSiddiqui23/pkg"
+)
 
 func main() {
-
-	pkg.Myname()
+	var a, b int = 1, 2
+	fmt.Println("1+2=", pkg.Add(a, b))
+	fmt.Println("2-1=", pkg.Subtract(b, a))
+	fmt.Println("1*2=", pkg.Multiply(a, b))
 }
